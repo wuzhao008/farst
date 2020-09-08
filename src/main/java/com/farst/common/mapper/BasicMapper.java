23a4d8e7f59c5e8b0bf679ad7550b71d63e040dc
MichaelWoo <86174413@qq.com>
2020-09-08 15:32:30 +0800

    redo module

--- src/main/java/com/farst/common/mapper/BasicMapper.java
+++
@@ -1,7 +0,0 @@
-package com.farst.common.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface BasicMapper<T> extends BaseMapper<T> {
-
-}
--- src/main/java/com/farst/common/service/IBasicService.java
+++
@@ -1,7 +0,0 @@
-package com.farst.common.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IBasicService<T> extends IService<T> {
-
-}
